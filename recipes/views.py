6a16be1b4e230a30b9789637435e13d31cfb7c01
Icaro Brauns <icaroferraribrauns@gmail.com>
2022-08-31 14:24:30 -0300

    exemplo template temp

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'temp/temp.html')
 
 
 def sobre(request):
