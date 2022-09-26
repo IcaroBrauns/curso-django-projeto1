e3393cd3120653671010c0d3929a6063668f4d9e
Icaro Brauns <icaroferraribrauns@gmail.com>
2022-09-26 11:50:30 -0300

    testando placeholders

--- authors/forms.py
+++ authors/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -12,6 +14,19 @@ def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
 
 
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, one '
+            'lowercase letter and one number. The length should be '
+            'at least 8 characters'
+        ),
+            code='Invalid'
+        )
+
+
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -19,30 +34,26 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['email'], 'Your email')
         add_placeholder(self.fields['first_name'], 'Your first name')
         add_placeholder(self.fields['last_name'], 'Your last name')
-        add_attr(self.fields['username'], 'css', 'a-css-class')
-
-    password2 = forms.CharField(
-        required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Repeat your password'
-            }
-        )
-    )
+        add_placeholder(self.fields['password'], 'Your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
 
     password = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(
-            attrs={
-                'placeholder': 'Your password'
-            }),
+        widget=forms.PasswordInput(),
         error_messages={
             'required': 'Password must not be empty'
         },
         help_text=(
             'Password must have at least one uppercase letter, one '
-            'lowercase letter and one number'
-        )
+            'lowercase letter and one number. The length should be '
+            'at least 8 characters'
+        ),
+        validators=[strong_password]
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput()
     )
 
     class Meta:
@@ -61,15 +72,6 @@ class RegisterForm(forms.ModelForm):
             'username': {
                 'required': 'This field must not be empty',
             }
-        }
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Type your first name here',
-                'class': 'input text-input'
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Type your password here'
-            })
         }
 
     def clean_password(self):
