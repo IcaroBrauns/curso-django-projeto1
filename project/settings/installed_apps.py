da8a4cdf2af0f53a73daf7d5760d269afcd64fb8
Icaro Brauns <icaroferraribrauns@gmail.com>
2022-10-25 17:08:29 -0300

    organizando arquivos de settings

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,11 @@
+INSTALLED_APPS = [
+    'authors',
+    'recipes',
+    'tag',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
