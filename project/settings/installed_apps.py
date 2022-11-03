ee94c3994d4fe6e19996d9a5ac331626625aa51d
Icaro Brauns <icaroferraribrauns@gmail.com>
2022-11-03 12:30:11 -0300

    tags serializers e api

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -1,11 +1,12 @@
 INSTALLED_APPS = [
-    'authors',
-    'recipes',
-    'tag',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'recipes',
+    'authors',
+    'tag',
 ]
